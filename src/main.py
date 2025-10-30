3d044f0b6bb5000d8c40f952e57d59868ee926c6
untill99 <gabriel.kasten@outlook.com>
2025-10-30 18:02:05 -0300

    feat: Add API documentation deployment workflow
    * Introduced a GitHub Actions workflow to deploy API documentation on push events.
    * Added a job to check API diff on pull requests.
    * Utilizes `bump-sh/github-action` for documentation handling.

--- src/main.py
+++ src/main.py
@@ -778,6 +778,8 @@ def remove_dashboard_from_group(
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
 
+## Associate Pipeline to Dashboard/Group
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
